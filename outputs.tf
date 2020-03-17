17182a4aea687f3e9526d414a1bcb4a97a6546ad
rhine59 <rhine59@gmail.com>
2020-03-17 17:11:47 +0000

    Tue 17 Mar 2020 17:11:47 GMT
    Signed-off-by: rhine59 <rhine59@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,9 @@
-# ibm_container_cluster_config
-output "id" {
-  value = data.ibm_container_cluster_config.cluster_config.id
+# ibm_container_cluster_config.cluster
+output "cluster_name_id" {
+  value = data.ibm_container_cluster_config.cluster_config.cluster_name_id
+}
+# ibm_container_cluster_config.cluster
+output "host" {
+  value = data.ibm_container_cluster_config.cluster_config.host
 }
 
